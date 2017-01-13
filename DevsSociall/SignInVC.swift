a2f515bdf7d6b72823f817d7b1d5aeb478f85bb4
Avinash Reddy <beavinash15@gmail.com>
2017-01-13 02:14:08 -0500

    Firebase Install and Setup. Change of Name in Project

---
+++ DevsSociall/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  DevsSociall
+//
+//  Created by Avinash Reddy on 1/13/17.
+//  Copyright © 2017 theEine. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
