db3f2046ccd56b45677f73a5c6bdf500eefec852
Avinash Reddy <beavinash15@gmail.com>
2017-01-14 11:20:46 -0500

    User Interface completed for home view

---
+++ DevsSociall/CustomCircleImageView.swift
@@ -0,0 +1,34 @@
+//
+//  CustomCircleImageView.swift
+//  DevsSociall
+//
+//  Created by Avinash Reddy on 1/14/17.
+//  Copyright © 2017 theEine. All rights reserved.
+//
+
+import UIKit
+
+class CustomCircleImageView: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
