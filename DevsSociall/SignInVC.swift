4faa813f437efd15fafeccbb3480ada2e85c6db6
Avinash Reddy <beavinash15@gmail.com>
2017-01-13 19:22:47 -0500

    Successfully able to authenticate with Facebook

--- DevsSociall/SignInVC.swift
+++ DevsSociall/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,35 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookButtonTapped(_ sender: Any) {
+        
+        // Facebook login
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Avinash Error: Unable to authenticate with Facebook: \(error)")
+            } else if result?.isCancelled == true { // to handle sudden cancelllation
+                
+                print("Avinash Error: User Cancelled Facebok Authentication")
+                
+            } else {
+                print("Avinash Succes: Succesfully authenticatd with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()!.signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("Avinash Error: Unable to authenticate with Firebase \(error?.localizedDescription)")
+            } else {
+                print("Avinash Success: Succesfully authenticated with Firebase")
+            }
+        }
+    }
+    
 
 }
 
